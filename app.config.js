fa30aaa7545ba25fef9dee75af47fa7c0f69e282
f80824568 <f80824568@gmail.com>
2025-06-01 06:26:05 -0500

    Deploy latest changes

---
+++ app.config.js
@@ -0,0 +1,9 @@
+export default ({ config }) => ({
+    ...config,
+    extra: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+      ...config.extra
+    },
+  });
+  
